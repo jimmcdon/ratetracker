fbd42e0c796c32f3ff8da786b2b0bf4bebcd5bfd
McD <mcd@m22.io>
2025-03-15 19:01:11 -0700

    Remove test pages causing build errors

--- app/mockup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { CalculatorMockup } from "@/components/calculator-mockup"
-
-export default function MockupPage() {
-  return (
-    <main className="min-h-screen bg-gradient-to-b from-white to-slate-50">
-      <CalculatorMockup />
-    </main>
-  )
-} 
--- app/target/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TargetCalculator } from "@/components/target-calculator"
-
-export default function TargetPage() {
-  return (
-    <div className="container py-8">
-      <TargetCalculator />
-    </div>
-  )
-} 
